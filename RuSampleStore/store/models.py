5124d3f96d4dbbbe11501c387325cc49dea80380
ilia sharin <x-tazz85@yandex.ru>
2023-02-02 15:21:54 +0400

    02-02_1

--- RuSampleStore/store/models.py
+++ RuSampleStore/store/models.py
@@ -3,10 +3,17 @@ from django.db import models
 
 class Sample(models.Model):
     title = models.TextField()
-    genre = models.CharField(max_length=255)
+    genre = models.ForeignKey('Genre', default=None, null=True, on_delete=models.SET_NULL)
     instrument = models.CharField(max_length=255)
     artist = models.TextField()
     samplepack = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Genre(models.Model):
+    title = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title
--- RuSampleStore/store/views.py
+++ RuSampleStore/store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from store.models import Sample
+
+
 # Create your views here.
+
+def main_page(request):
+    return render(request, 'index.html', {'samples': Sample.objects.all})
