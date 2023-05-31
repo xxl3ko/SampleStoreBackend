5545385ec8919d12131d80dff399ef99ba138ee1
ilia sharin <x-tazz85@yandex.ru>
2023-05-31 13:48:26 +0400

    05-31 iMac afternoon

--- RuSampleStore/base/services.py
+++ RuSampleStore/base/services.py
@@ -4,3 +4,8 @@ def get_path_upload_sample(instance, file):
 
 def get_path_upload_cover(instance, file):
     return f'{instance.label}/{instance.name}/info/{file}'
+
+
+"""def get_path_upload_cover(instance, file):
+    return f'{instance.label}/{instance.name}/info/{file}'
+"""
